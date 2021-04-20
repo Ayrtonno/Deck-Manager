c497873379c649be2750466c9baabae8cff6d1dd
Ayrtonno <ayrtonpizzi@libero.it>
2021-04-20 21:18:44 +0200

    init frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,50 @@
-const { useState, useEffect } = require("react")
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-const App = ()=> {
+const Deck = (props) => {
+    return (
+        <div>
+            {props.name}
+            {" "}
+            {props.type}
+            {" "}
+            {props.cardNumber}
+            {" "}
+            {props.price}
+        </div>
+    )
+}
+
+// per react quest "App" è un componente funzionale perche è una funzione di per sè.
+const App = () => {
     const [counter, setCounter] = useState(0)
-    useEffect(()=> {
-        if(counter === 10){
+
+    const [decks, setDecks] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:8080/deck").then((response) => setDecks(response.data))
+    })
+
+    useEffect(() => {
+        if (counter === 10) {
             console.log("Bravo scemo")
         }
-    },[counter])
-return(
-    <div>
-        <button onClick={()=> setCounter((current)=>counter+1)}>
-            +1
-        </button> 
-        <button onClick={() => setCounter((current) => counter - 1)}>
-            -1
-        </button>
-        <span>
-            {counter}
-        </span>
-    </div>
-)}
+        // [counter] è un array delle dipendenza, quando cambia una di queste variabili viene richiamata la funzione all'interno
+    }, [counter])
+    return (
+        <div>
+            <button onClick={() => setCounter((current) => current + 1)}>
+                +1
+            </button>
+            <button onClick={() => setCounter((current) => current - 1)}>
+                -1
+            </button>
+            <span>
+                {counter}
+            </span>
+            <div>
+                {decks.map((deck) => <Deck name={deck.name} type={deck.type} cardNumber={deck.cardNumber} price={deck.price} />)}
+            </div>
+        </div>
+    )
+}
 export default App
